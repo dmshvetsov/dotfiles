a3b9e8794bc2c51a16a035ac730397df3579157c
Dmitry Shvetsov <d24v@192.168.1.20>
2022-12-04 19:29:33 +0800

    remplace zellij configs with wezterm

---
+++ dotconfig/wezterm/wezterm.lua
@@ -0,0 +1,13 @@
+local wezterm = require 'wezterm'
+
+return {
+  color_scheme = 'embark'
+  font = wezterm.font 'JetBrains Mono'
+  font_size = 13,
+  widow_padding = {
+    left = 0,
+    right = 0,
+    top = 0,
+    bottom  = 0,
+  }
+}
