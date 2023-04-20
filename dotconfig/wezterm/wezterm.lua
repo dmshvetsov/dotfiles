c6ccda840549cd07a261be1ac3584789286a7b5f
dmshvetsov <shvetsovdm@gmail.com>
2023-04-20 15:34:42 +0300

    fix typo in wezterm conf

--- dotconfig/wezterm/wezterm.lua
+++ dotconfig/wezterm/wezterm.lua
@@ -4,7 +4,7 @@ return {
   color_scheme = 'embark',
   font = wezterm.font 'JetBrains Mono',
   font_size = 13,
-  widow_padding = {
+  window_padding = {
     left = 0,
     right = 0,
     top = 0,
