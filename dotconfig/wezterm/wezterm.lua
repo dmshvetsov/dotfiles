a3eea5e15ee7bfc87216bcdbf6371d0c8fe0b5ea
dmshvetsov <shvetsovdm@gmail.com>
2023-06-26 07:55:38 +0200

    set wezterm font weight to medium
    this make fonts looks better on non-retina displays

--- dotconfig/wezterm/wezterm.lua
+++ dotconfig/wezterm/wezterm.lua
@@ -2,7 +2,7 @@ local wezterm = require 'wezterm'
 
 return {
   color_scheme = 'embark',
-  font = wezterm.font 'JetBrains Mono',
+  font = wezterm.font { family = 'JetBrains Mono', weight = 'Medium' },
   font_size = 13,
   window_padding = {
     left = '0.5cell',
