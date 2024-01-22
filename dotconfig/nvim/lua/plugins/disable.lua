25aaa67cdc4e0bd3bc70f1d31e7ced11e74eb99c
dmshvetsov <shvetsovdm@gmail.com>
2024-01-22 13:00:02 +0200

    neovim: disable mini.pairs plugin

--- dotconfig/nvim/lua/plugins/disable.lua
+++ dotconfig/nvim/lua/plugins/disable.lua
@@ -1,7 +1,14 @@
 -- disabled lazyvim plugins
 return {
   {
+    -- do not like that it opens on nvim start
     "nvim-neo-tree/neo-tree.nvim",
+    enabled = false,
+  },
+  {
+    -- do not likea when it inserts pair of quotes befor string start or end
+    -- can be fixed with config
+    "echasnovski/mini.pairs",
     enabled = false,
   },
 }
