23e56ab6355baf281f32b7a07acdbb1b606c69e8
dmshvetsov <shvetsovdm@gmail.com>
2024-01-24 11:33:04 +0200

    neovim: disable plugins autoupdate feature

--- dotconfig/nvim/lua/config/lazy.lua
+++ dotconfig/nvim/lua/config/lazy.lua
@@ -30,7 +30,8 @@ require("lazy").setup({
     -- version = "*", -- try installing the latest stable version for plugins that support semver
   },
   -- install = { colorscheme = { "tokyonight", "habamax" } },
-  checker = { enabled = true }, -- automatically check for plugin updates
+  -- plugins autoupdates checker
+  checker = { enabled = false },
   performance = {
     rtp = {
       -- disable some rtp plugins
