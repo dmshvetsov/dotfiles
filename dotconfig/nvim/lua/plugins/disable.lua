24edb43143f0c1ce716a56dbeecd28b780e7177a
dmshvetsov <shvetsovdm@gmail.com>
2024-01-27 12:09:52 +0200

    neovim: rewrite disabled lua config
    with usage of lua table and pairs iterator, just for fun

--- dotconfig/nvim/lua/plugins/disable.lua
+++ dotconfig/nvim/lua/plugins/disable.lua
@@ -1,23 +1,21 @@
 -- disabled lazyvim builtin plugins
-return {
-  {
-    -- do not like that it opens on nvim start
-    "nvim-neo-tree/neo-tree.nvim",
-    enabled = false,
-  },
-  {
-    -- do not likea when it inserts pair of quotes befor string start or end
-    -- can be fixed with config
-    "echasnovski/mini.pairs",
-    enabled = false,
-  },
-  {
-    "akinsho/bufferline.nvim",
-    enabled = false,
-  },
-  {
-    -- do not like flash jump feature in vim search
-    "folke/flash.nvim",
-    enabled = false,
-  },
+local disabled_plugins = {
+  -- do not like that it opens on nvim start
+  "nvim-neo-tree/neo-tree.nvim",
+  -- do not likea when it inserts pair of quotes befor string start or end
+  -- can be fixed with config
+  "echasnovski/mini.pairs",
+  -- do not use it and it takes spance and resources
+  "akinsho/bufferline.nvim",
+  -- do not like flash jump feature in vim search
+  "folke/flash.nvim",
 }
+
+local conf = {}
+for _, plugin_name in pairs(disabled_plugins) do
+  table.insert(conf, {
+    plugin_name,
+    enabled = false,
+  })
+end
+return conf
