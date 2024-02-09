79d044669381b2095d91105130b75263df2b33ee
dmshvetsov <shvetsovdm@gmail.com>
2024-02-09 11:18:41 +0200

    neovim: enable back neo-tree plugin
    Change options to not open it by default.
    There might be a better way to do it,
    see this discussion https://github.com/nvim-neo-tree/neo-tree.nvim/discussions/1325

--- dotconfig/nvim/lua/plugins/disable.lua
+++ dotconfig/nvim/lua/plugins/disable.lua
@@ -1,7 +1,5 @@
 -- disabled lazyvim builtin plugins
 local disabled_plugins = {
-  -- do not like that it opens on nvim start
-  "nvim-neo-tree/neo-tree.nvim",
   -- do not likea when it inserts pair of quotes befor string start or end
   -- can be fixed with config
   "echasnovski/mini.pairs",
