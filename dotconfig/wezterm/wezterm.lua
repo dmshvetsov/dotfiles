509d6d163612023afb5114b7cc26940b9abb802f
dmshvetsov <shvetsovdm@gmail.com>
2024-02-28 14:22:21 +0200

    wezterm: reformat the config

--- dotconfig/wezterm/wezterm.lua
+++ dotconfig/wezterm/wezterm.lua
@@ -1,13 +1,13 @@
-local wezterm = require 'wezterm'
+local wezterm = require("wezterm")
 
 return {
-  color_scheme = 'embark',
-  font = wezterm.font { family = 'JetBrains Mono', weight = 'Medium' },
-  font_size = 13,
-  window_padding = {
-    left = '0.5cell',
-    right = '0.5cell',
-    top = '0.1cell',
-    bottom  = 0,
-  }
+	color_scheme = "embark",
+	font = wezterm.font({ family = "JetBrains Mono", weight = "Medium" }),
+	font_size = 13,
+	window_padding = {
+		left = "0.5cell",
+		right = "0.5cell",
+		top = "0.1cell",
+		bottom = 0,
+	},
 }
