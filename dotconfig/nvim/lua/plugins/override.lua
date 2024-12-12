50927fb45bb168e8a71ea1a68716e0771b2b442c
dmshvetsov <shvetsovdm@gmail.com>
2024-12-12 16:49:44 +0200

    neovim: autoclose neo-tree on open file

--- dotconfig/nvim/lua/plugins/override.lua
+++ dotconfig/nvim/lua/plugins/override.lua
@@ -9,6 +9,15 @@ return {
       window = {
         position = "right",
       },
+      event_handlers = {
+        {
+          event = "file_open_requested",
+          handler = function()
+            -- auto close
+            require("neo-tree.command").execute({ action = "close" })
+          end,
+        },
+      },
     },
   },
 }
