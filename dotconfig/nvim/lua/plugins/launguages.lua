146f712fc37fa7e272076aa3c5db7e6f59cb2963
dmshvetsov <shvetsovdm@gmail.com>
2025-01-07 19:49:08 +0200

    neovim: add prisma schema lang support

--- dotconfig/nvim/lua/plugins/launguages.lua
+++ dotconfig/nvim/lua/plugins/launguages.lua
@@ -2,5 +2,7 @@ return {
   {
     -- Move language support
     "rvmelkonian/move.vim",
+    -- Prisma schema language file detection and syntax highlighting support
+    "prisma/vim-prisma",
   },
 }
