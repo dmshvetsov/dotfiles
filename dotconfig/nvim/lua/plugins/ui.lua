193a1303d08c9d26a2ba791cef81d11a83e9b148
dmshvetsov <shvetsovdm@gmail.com>
2025-02-13 11:12:49 +0200

    nvim: replace catppuccin with poimandres colorscheme

--- dotconfig/nvim/lua/plugins/ui.lua
+++ dotconfig/nvim/lua/plugins/ui.lua
@@ -1,7 +1,23 @@
 return {
   { "dmshvetsov/neo-colorscheme.nvim" },
   { "embark-theme/vim" },
-  { "catppuccin/nvim", name = "catppuccin", priority = 1000 },
+  {
+    "olivercederborg/poimandres.nvim",
+    lazy = false,
+    priority = 1000,
+    config = function()
+      require('poimandres').setup {
+        -- leave this setup function empty for default config
+        -- or refer to the configuration section
+        -- for configuration options
+      }
+    end,
+
+    -- optionally set the colorscheme within lazy config
+    -- init = function()
+    --   vim.cmd("colorscheme poimandres")
+    -- end
+  },
 
   {
     "LazyVim/LazyVim",
