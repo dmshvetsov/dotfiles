b5fc12b5cfa0e4b2ab844fcfcae228a169d97b0a
dmshvetsov <shvetsovdm@gmail.com>
2025-05-14 13:53:43 +0200

    neovim: update LSP configuration
    no need for `ensure_installed`, LazyVim will do it if a LSP config was
    added to the `servers`
    
    add new move-analyzer LSP config for Sui
    
    disable `typos-lsp` and `harper-ls`, too much diagnostic warnings

--- dotconfig/nvim/lua/plugins/lsp.lua
+++ dotconfig/nvim/lua/plugins/lsp.lua
@@ -1,17 +1,9 @@
+local home_dir = vim.fn.expand("~")
+
 return {
   {
-    "williamboman/mason.nvim",
-    opts = {
-      ensure_installed = {
-        "stylua",
-        "typos-lsp",
-        "harper-ls",
-        "tailwindcss-language-server",
-      },
-    },
-  },
-  {
     "neovim/nvim-lspconfig",
+    ---@class PluginLspOpts
     opts = {
       -- Enable this to enable the builtin LSP inlay hints on Neovim >= 0.10.0
       -- Be aware that you also will need to properly configure your LSP server to
@@ -19,32 +11,58 @@ return {
       inlay_hints = {
         enabled = false,
       },
+
+      --
+      -- Language servers configuration overrides, default configs are provided by nvim-lspconfig plugin
+      --
+      -- any entry in "servers" table will be instead automatically, provided configuration will be merged with the nvim-lspconfig
+      --
+
+      ---@type lspconfig.options
       servers = {
-        -- NOTE: `harper-ls` and `typos-lsp` may provide duplicate or even conflicting warnings/suggestions
+        -- disable `typos-lsp` in favour of `harper-ls` may provide duplicate or even conflicting warnings/suggestions
         -- Typos spell checker config https://github.com/tekumara/typos-lsp/blob/main/docs/neovim-lsp-config.md
-        typos_lsp = {
-          init_options = {
-            diagnosticSeverity = "Hint",
-          },
-        },
+        -- typos_lsp = {
+        --   init_options = {
+        --     diagnosticSeverity = "Hint",
+        --   },
+        -- },
         -- configuration docs https://writewithharper.com/docs/integrations/neovim
-        harper_ls = {
-          settings = {
-            ["harper-ls"] = {
-              linters = {
-                SentenceCapitalization = false,
-              },
-            },
-          },
-        },
+        -- harper_ls = {
+        --   settings = {
+        --     ["harper-ls"] = {
+        --       linters = {
+        --         SentenceCapitalization = false,
+        --       },
+        --     },
+        --   },
+        -- },
         -- end of `harper-ls` and `typos-lsp` NOTE
+
+        tailwindcss = {},
+
+        -- Move analyzer for Sui
+        -- not supported by Mason, Sui CLI + move-analyzer myst be installed manyally
+        move_analyzer = {
+          -- disable automatically installing the server by LazyVim and Mason
+          mason = false,
+          cmd = { home_dir .. "/.sui/bin/move-analyzer" },
+          -- settings = {
+          --   server = {
+          --     path = '~/.sui/bin/move-analyzer'
+          --   }
+          -- }
+        },
+
+        -- Typescript (must be disbaled for Javascript projects)
         vtsls = {
           root_dir = require("lspconfig").util.root_pattern({ "package.json", "tsconfig.json" }),
           single_file_support = false,
         },
+
         -- Deno
         denols = {
-          root_dir = require("lspconfig").util.root_pattern({"deno.json", "deno.jsonc"}),
+          root_dir = require("lspconfig").util.root_pattern({ "deno.json", "deno.jsonc" }),
           single_file_support = false,
           settings = {},
         },
