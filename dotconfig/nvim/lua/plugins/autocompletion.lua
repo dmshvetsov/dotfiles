02b130541a0ad529528f2a9420f05ff545a68507
dmshvetsov <shvetsovdm@gmail.com>
2025-07-08 20:21:22 +0200

    neovim: disable auto brackets for fn on auto completion

---
+++ dotconfig/nvim/lua/plugins/autocompletion.lua
@@ -0,0 +1,21 @@
+return {
+{
+  "saghen/blink.cmp",
+  opts = {
+    completion = {
+      accept = {
+        auto_brackets = {
+          -- disable adding brackets for funcitons
+          kind_resolution = {
+            enabled = false
+          },
+          -- disable adding brackets for funcitons
+          semantic_token_resolution = {
+            enabled = false
+          }
+        }
+      }
+    }
+  }
+}
+}
