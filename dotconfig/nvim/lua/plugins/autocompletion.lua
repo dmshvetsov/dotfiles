c74d0f28aa3c59ef7e28608cf59ea3b74408714e
dmshvetsov <shvetsovdm@gmail.com>
2025-10-31 08:18:08 +0200

    neovim: remove snippets from default autocompletion options

--- dotconfig/nvim/lua/plugins/autocompletion.lua
+++ dotconfig/nvim/lua/plugins/autocompletion.lua
@@ -10,10 +10,32 @@ return {
         ---   `show` on C-p or C-n
         ["<C-p>"] = { "show", "select_prev", "fallback_to_mappings" },
         ["<C-n>"] = { "show", "select_next", "fallback_to_mappings" },
+        -- snipets
+        ["<Tab>"] = {
+          function(cmp)
+            -- if cmp.snippet_active() then
+            --   return cmp.accept()
+            -- else
+            --   return cmp.select_and_accept()
+            -- end
+            if not cmp.snippet_active() then
+              return cmp.show({ providers = { "snippets" } })
+            else
+              -- TODO: does not work
+              return not cmp.select_next();
+            end
+          end,
+          -- "snippet_forward",
+          "fallback",
+        },
+        -- ["<S-Tab>"] = { "snippet_backward", "fallback" },
       },
       sources = {
         -- NOTE: lazyvim uses nvim_lsp in docs for blimk.cmp configuratin but it is not in `providers`
-        default = { "lsp", "path", "snippets", "buffer", "codeium" },
+        -- NOTE: function is need to prevent layvim to update it https://github.com/LazyVim/LazyVim/discussions/5599
+        default = function()
+          return { "lsp", "path", "buffer", "codeium" }
+        end,
         per_filetype = {
           sql = { "dadbod" },
           -- optionally inherit from the `default` sources
